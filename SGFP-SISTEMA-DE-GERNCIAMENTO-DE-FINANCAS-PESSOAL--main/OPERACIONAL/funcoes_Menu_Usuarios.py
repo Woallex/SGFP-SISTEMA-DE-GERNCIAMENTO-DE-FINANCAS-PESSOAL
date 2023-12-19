51b796d1b62c2754319e8f0bd72444df596fb1f3
Alex Pereira <144354406+Woallex@users.noreply.github.com>
2023-12-18 21:12:29 -0300

    Update funcoes_Menu_Usuarios.py

--- SGFP-SISTEMA-DE-GERNCIAMENTO-DE-FINANCAS-PESSOAL--main/OPERACIONAL/funcoes_Menu_Usuarios.py
+++ SGFP-SISTEMA-DE-GERNCIAMENTO-DE-FINANCAS-PESSOAL--main/OPERACIONAL/funcoes_Menu_Usuarios.py
@@ -1,6 +1,12 @@
 import sqlite3
 import main
+import os
+import time
 import user 
+
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def configuracoes_usuario(user_id, conn):
     
     while True:
@@ -26,11 +32,16 @@ def configuracoes_usuario(user_id, conn):
                 novo_nome = input(f"Nome atual: {nome_atual}\nDigite o novo nome: ")
                 if novo_nome == "":
                     print("Você esta inserindo um campo em branco. Por favor tente novamente.")
+                    time.sleep(2)
                     configuracoes_usuario(user_id, conn)
                 atualizar_nome_usuario(conn, user_id, novo_nome)
                 print("Nome atualizado com sucesso!")
+                time.sleep(2)
+                limpar_tela()
             else:
                 print("Senha incorreta. Tente novamente.")
+                time.sleep(2)
+                limpar_tela()
 
         elif escolha == '2':
             senha = user.getpass("Digite a sua senha atual: ")
@@ -38,11 +49,16 @@ def configuracoes_usuario(user_id, conn):
                 novo_cpf = input(f"CPF atual: {cpf_atual}\nDigite o novo CPF: ")
                 if novo_cpf == "":
                     print("O CPF não pode ser um campo em branco. Tente novamente.")
+                    time.sleep(2)
                     configuracoes_usuario(user_id, conn)
                 atualizar_cpf_usuario(conn, user_id, novo_cpf)
                 print("CPF atualizado com sucesso!")
+                time.sleep(2)
+                limpar_tela()
             else:
                 print("Senha incorreta. Tente novamente.")
+                time.sleep(2)
+                limpar_tela()
 
         elif escolha == '3':
             senha = user.getpass("Digite a sua senha atual: ")
@@ -50,36 +66,49 @@ def configuracoes_usuario(user_id, conn):
                 nova_senha = input("Digite a nova senha: ")
                 if nova_senha == "":
                     print("Você esta inserindo um campo em branco. Por favor tente novamente.")
+                    time.sleep(2)
                     configuracoes_usuario(user_id, conn)
                 confirmar_senha = user.getpass("Confirme a nova senha: ")
                 if nova_senha == confirmar_senha:
                     atualizar_senha_usuario(conn, user_id, nova_senha)
                     print("Senha atualizada com sucesso!")
+                    time.sleep(2)
+                    limpar_tela()
                 else:
                     print("As senhas não coincidem. Tente novamente.")
+                    time.sleep(2)
             else:
                 print("Senha incorreta. Tente novamente.")
+                time.sleep(2)
 
         elif escolha == '4':
             senha = user.getpass("Digite a sua senha atual para confirmar a exclusão: ")
             if senha =="":
                 print("Você esta inserindo um espaço em branco. Por favor tente novamente.")
+                time.sleep(2)
                 configuracoes_usuario(user_id, conn)
             conf_senha = user.getpass("Confirme sua senha: ")
+            time.sleep(2)
             if conf_senha == senha:
                 if verificar_senha(conn, user_id, senha):
                     deletar_usuario(conn, user_id)
                     print("Usuário deletado com sucesso!")
+                    time.sleep(2)
+                    limpar_tela()
                     main.main()
                     return
             else:
                 print("Senha incorreta. A exclusão não foi realizada.")
+                time.sleep(2)
+                limpar_tela()
         
         elif escolha == '5':
             break
 
         else:
             print("Opção inválida. Tente novamente.")
+            time.sleep(2)
+            limpar_tela()
 
 
 def atualizar_nome_usuario(conn, user_id, novo_nome):
@@ -89,6 +118,7 @@ def atualizar_nome_usuario(conn, user_id, novo_nome):
         conn.commit()
     except sqlite3.Error as e:
         print(f"Erro ao atualizar o nome do usuário: {e}")
+        time.sleep(2)
 
 def atualizar_cpf_usuario(conn, user_id, novo_cpf):
     try:
@@ -97,6 +127,7 @@ def atualizar_cpf_usuario(conn, user_id, novo_cpf):
         conn.commit()
     except sqlite3.Error as e:
         print(f"Erro ao atualizar o CPF do usuário: {e}")
+        time.sleep(2)
 
 def atualizar_senha_usuario(conn, user_id, nova_senha):
     try:
@@ -105,6 +136,7 @@ def atualizar_senha_usuario(conn, user_id, nova_senha):
         conn.commit()
     except sqlite3.Error as e:
         print(f"Erro ao atualizar a senha do usuário: {e}")
+        time.sleep(2)
 
 def deletar_usuario(conn, user_id):
     try:
@@ -113,6 +145,7 @@ def deletar_usuario(conn, user_id):
         conn.commit()
     except sqlite3.Error as e:
         print(f"Erro ao deletar o usuário: {e}")
+        time.sleep(2)
         
 def verificar_senha(conn, user_id, senha):
     try:
@@ -126,4 +159,5 @@ def verificar_senha(conn, user_id, senha):
             return False
     except sqlite3.Error as e:
         print(f"Erro ao verificar a senha: {e}")
+        time.sleep(2)
         return False
